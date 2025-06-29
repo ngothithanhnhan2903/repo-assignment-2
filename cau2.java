4384cc8b56b8a67f4d601bce212e7afda20c978b
Nhan Thanh <Nhanngo.290301@gmail.com>
2025-06-30 00:13:56 +0700

    Implement MULTIPLY operation

--- cau2.java
+++ cau2.java
@@ -1,10 +1,13 @@
 private Double calculate(SIGN sign, Double arg1, Double arg2) {
     switch (sign) {
         default:
-            case PLUS:
-                return arg1 + arg2;
+            case MULTIPLY:
+                return arg1 * arg2;
             default
             break;
+            if (operator === "*") {
+                result = a * b;
+            }
     }
     return 0d;
 }
