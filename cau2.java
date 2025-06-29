8bdef61383f9f18fec3c82d49450c0af25c68efb
Nhan Thanh <Nhanngo.290301@gmail.com>
2025-06-30 00:02:26 +0700

    Implement MINUS operation

--- cau2.java
+++ cau2.java
@@ -1,8 +1,8 @@
 private Double calculate(SIGN sign, Double arg1, Double arg2) {
     switch (sign) {
         default:
-            case PLUS:
-                return arg1 + arg2;
+            case MINUS:
+                return arg1 - arg2;
             default
             break;
     }
